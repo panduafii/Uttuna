f1cabd22e3638f65172ee0cc413f90f1e9e8bca9
Pandu Dewanto <panduafii@MacBook-Pro-Pandu.local>
2023-12-07 14:53:27 +0700

    modified:   CSS/style.css         modified:   index.php

--- index.php
+++ index.php
@@ -16,20 +16,13 @@
       crossorigin="anonymous"
     />
     <!-- Font Alfath -->
-    <link
-      href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;500;700&display=swap"
-      rel="stylesheet"
-    />
-
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;500;700&display=swap" rel="stylesheet"/>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;500;700&family=Vollkorn:wght@400;600&display=swap" rel="stylesheet">
     <!-- Font -->
     <!-- Icon Alfath -->
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
-    />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"/>
     <!--Icon  -->
 
     <title>Home</title>

7641652d7184ac10e92493f1c1124ccf1527c5cc
fathiza <fathriza563@gmail.com>
2023-12-11 01:39:40 +0700

    menambahkan responsive navbar pada profile dan juga ubah warna logo

--- Profile.php
+++ Profile.php
@@ -7,13 +7,19 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="CSS/Profile.css">
+    <link rel="stylesheet" href="./CSS/profile.css"/>
 
     <!-- Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;500;700;800;900&display=swap" rel="stylesheet">
 
+    <!-- Icon -->
+    <link
+      rel="stylesheet"
+      href="https://unicons.iconscout.com/release/v4.0.8/css/line.css"
+    />
+    <!--  -->
 </head>
 
 <body>
@@ -29,6 +35,34 @@
     }
     include("fungsiPHP/check-profile.php");
     ?>
+    <!-- header by pandu -->
+      <header>
+        <div class="nav-bar">
+          <a href="index.php" class="logo"><img src="./img/logo5.png" /></a>
+          <div class="navigation">
+            <div class="nav-items">
+              <i class="uil uil-times nav-close-btn"></i>
+              <a href="index.php"><i class="uil uil-home"></i>Home</a>
+              <a href="index.php"><i class="uil uil-compass"></i>Destination</a>
+              <a href="#"><i class="uil uil-document-layout-left"></i>Explore</a>
+              <a href="index.php"><i class="uil uil-info-circle"></i>About Us</a>
+              <?php
+              // Cek apakah pengguna sudah login
+              if (isset($_SESSION['username'])) {
+                // Jika sudah login, tampilkan tautan ke profil dan logout
+                echo '<a href="profile.php"><i class="uil uil-user"></i>Profile</a>';
+                echo '<a href="fungsiPHP/check-logout.php"><i class="uil uil-sign-out-alt"></i></i>Logout</a>';
+              } else {
+                // Jika belum login, tampilkan tautan ke halaman login
+                 echo '<a class="loginHeader" href="Login1.php"><i class="uil uil-user"></i>Login</a>';
+              }
+              ?>
+            </div>
+          </div>
+          <i class="uil uil-apps nav-menu-btn"></i>
+        </div>
+      </header>
+      <!-- ==== -->
     <!-- Atas -->
     <div class="atas">
         <h1>WONDERFUL NATUNA</h1>
@@ -79,6 +113,7 @@
     <!-- Import Icon -->
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <script src="js/profile.js"></script>
 
     </div>
 </body>
--- js/profile.js
+++ js/profile.js
@@ -1,40 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const showPopupBtn = document.getElementById("showPopupBtn");
-    const closePopupBtn = document.getElementById("closePopupBtn");
-    const myPopup = document.getElementById("myPopup");
-    const overlay = document.getElementById("overlay");
-  
-    showPopupBtn.addEventListener("click", function () {
-      myPopup.style.display = "block";
-      overlay.style.display = "block";
-    });
-  
-    closePopupBtn.addEventListener("click", function () {
-      myPopup.style.display = "none";
-      overlay.style.display = "none";
-    });
-  
-    overlay.addEventListener("click", function () {
-      myPopup.style.display = "none";
-      overlay.style.display = "none";
-    });
+  const showPopupBtn = document.getElementById("showPopupBtn");
+  const closePopupBtn = document.getElementById("closePopupBtn");
+  const myPopup = document.getElementById("myPopup");
+  const overlay = document.getElementById("overlay");
+
+  showPopupBtn.addEventListener("click", function () {
+    myPopup.style.display = "block";
+    overlay.style.display = "block";
   });
-  
-  window.addEventListener("scroll", function () {
-    const header = this.document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
+
+  closePopupBtn.addEventListener("click", function () {
+    myPopup.style.display = "none";
+    overlay.style.display = "none";
   });
-  
-  //Responsive navigation menu toggle
-  const menuBtn = document.querySelector(".nav-menu-btn");
-  const closeBtn = document.querySelector(".nav-close-btn");
-  const navigation = document.querySelector(".navigation");
-  
-  menuBtn.addEventListener("click", () => {
-    navigation.classList.add("active");
+
+  overlay.addEventListener("click", function () {
+    myPopup.style.display = "none";
+    overlay.style.display = "none";
   });
-  
-  closeBtn.addEventListener("click", () => {
-    navigation.classList.remove("active");
-  });
-  
+});
+
+window.addEventListener("scroll", function () {
+  const header = this.document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+//Responsive navigation menu toggle
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+
+menuBtn.addEventListener("click", () => {
+  navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  navigation.classList.remove("active");
+});
